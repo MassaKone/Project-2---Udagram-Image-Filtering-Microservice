e92c6c9b289466a0b41b01fc888caed092dad7d2
Massa Koné <36091977+MassaKone@users.noreply.github.com>
2022-08-26 23:40:25 -0400

    Added Axios to help Jimp promises
    Added Axios to resolve "Could not find MIME for Buffer" for certain type of URLs

--- src/server.ts
+++ src/server.ts
@@ -13,24 +13,7 @@ import {filterImageFromURL, deleteLocalFiles, isValidURL} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-
+  // "/filteredimage" enpoint to get image URL from query, download image, filter it and resend to the client
   app.get( "/filteredimage/", async ( req: express.Request, res: express.Response) => {
     try{
       // Get the image URL from the query
@@ -55,7 +38,7 @@ import {filterImageFromURL, deleteLocalFiles, isValidURL} from './util/util';
       // Use helper function to delete all files
       res.on('finish', async () => await deleteLocalFiles(files));
     } catch (error){
-        return res.status(500).send(e.message);
+        return res.status(500).send(error.message);
     }
 
   } );
